1f2eac865fe2ac353345d23c75db33f02ad3898d
zechengh <zcheustc@gmail.com>
2020-01-21 12:12:44 -0500

    record

--- exp/l1pp/run_victim.sh
+++ exp/l1pp/run_victim.sh
@@ -11,7 +11,7 @@ OUTPUT_FOLDER=$EXP_ROOT_DIR/l1pp/results
 mkdir -p $OUTPUT_FOLDER
 
 status "Run victim"
-taskset 0x8 ./victim &
+taskset 0x8 ./victim
 VICTIM_PID=$!
 echo $VICTIM_PID
 
@@ -20,3 +20,4 @@ $quickhpc -c hpc_config -a $VICTIM_PID -i 1000 > $OUTPUT_FOLDER/hpc_victim
 sleep 10
 
 kill $VICTIM_PID
+echo "Experiment ends"
