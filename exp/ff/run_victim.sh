26bcd66f13fee0fe3db81cb8ac3fdadbecd048a8
zechengh <zcheustc@gmail.com>
2020-01-21 23:25:27 -0500

    ff attack

--- exp/ff/run_victim.sh
+++ exp/ff/run_victim.sh
@@ -19,18 +19,6 @@ while true;
   do
     taskset 0x4 $GPG -r 'zechengh_key1' -d 'hello.txt.gpg' &
     VICTIM_PID=$!
-    $quickhpc -c hpc_config -a $VICTIM_PID -i 100
+#    $quickhpc -c hpc_config -a $VICTIM_PID -i 100
     sleep 1
   done
-
-#status "Attacker starts"
-#./attacker $SHARED_MEM > $OUTPUT_FOLDER/0
-
-#status "Victim starts"
-#./victim $SHARED_MEM &
-#VICTIM_PID=$!
-
-#./attacker $SHARED_MEM > $OUTPUT_FOLDER/1
-
-#kill "$VICTIM_PID"
-#status "Experiment ends"
--- exp/flush_reload/run_victim.sh
+++ exp/flush_reload/run_victim.sh
@@ -19,7 +19,7 @@ while true;
   do
     taskset 0x4 $GPG -r 'zechengh_key1' -d 'hello.txt.gpg' &
     VICTIM_PID=$!
-    $quickhpc -c hpc_config -a $VICTIM_PID -i 100
+#    $quickhpc -c hpc_config -a $VICTIM_PID -i 100
     sleep 1
   done
 
