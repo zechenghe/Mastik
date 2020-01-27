ebf02205d660ffcddc01faac15c0743057439128
zechengh <zcheustc@gmail.com>
2020-01-27 13:04:48 -0500

    hpc suffix

--- exp/l1pp/run_sensitive.sh
+++ exp/l1pp/run_sensitive.sh
@@ -11,32 +11,37 @@ mkdir -p $OUTPUT_FOLDER
 
 GPG=$ROOT_DIR/gnupg-1.4.13/g10/gpg
 
-status "Sensitive program running"
-taskset 0x8000 ./sensitive1 &
-SENSITIVE_PID=$!
-echo $SENSITIVE_PID
+for HPC_SUFFIX in L1 L23 MISC
+do
 
-$quickhpc -c hpc_config_2 -a $SENSITIVE_PID -i 100 > $OUTPUT_FOLDER/hpc_sensiprog &
-QUICKHPC_PID=$!
+  status "Sensitive program running"
+  taskset 0x8000 ./sensitive1 &
+  SENSITIVE_PID=$!
+  echo $SENSITIVE_PID
 
-sleep 5
-kill $QUICKHPC_PID
-kill $SENSITIVE_PID
+  $quickhpc -c hpc_config_$HPC_SUFFIX -a $SENSITIVE_PID -i 100 > $OUTPUT_FOLDER/hpc_sensiprog_$HPC_SUFFIX &
+  QUICKHPC_PID=$!
+
+  sleep 5
+  kill $QUICKHPC_PID
+  kill $SENSITIVE_PID
 
 
-status "Spy running"
-taskset 0x8000 ./spy 1000000000 &
-SPY_PID=$!
+  status "Spy running"
+  taskset 0x8000 ./spy 1000000000 &
+  SPY_PID=$!
 
-status "Sensitive program running"
-taskset 0x8 ./sensitive1 &
-SENSITIVE_PID=$!
-echo $SENSITIVE_PID
+  status "Sensitive program running"
+  taskset 0x8 ./sensitive1 &
+  SENSITIVE_PID=$!
+  echo $SENSITIVE_PID
 
-$quickhpc -c hpc_config_2 -a $SENSITIVE_PID -i 100 > $OUTPUT_FOLDER/hpc_sensiprog_abnormal &
-QUICKHPC_PID=$!
+  $quickhpc -c hpc_config_$HPC_SUFFIX -a $SENSITIVE_PID -i 100 > $OUTPUT_FOLDER/hpc_sensiprog_abnormal_$HPC_SUFFIX &
+  QUICKHPC_PID=$!
 
-sleep 5
-kill $QUICKHPC_PID
-kill $SENSITIVE_PID
-kill $SPY_PID
+  sleep 5
+  kill $QUICKHPC_PID
+  kill $SENSITIVE_PID
+  kill $SPY_PID
+
+done
