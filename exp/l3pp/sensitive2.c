1f45850dd3d83739510cfd328038cabb86f29dca
zechengh <zcheustc@gmail.com>
2020-02-04 21:17:43 -0500

    debug

--- exp/l3pp/sensitive2.c
+++ exp/l3pp/sensitive2.c
@@ -9,8 +9,8 @@ volatile char buffer[PAGE_SIZE*1000];
 int main(int ac, char **av) {
   for (;;) {
     for (int i = 0; i < PAGE_SIZE; i++){
-      buffer[i*PAGE_SIZE + 31] += i
-      buffer[i*PAGE_SIZE + 168] += i
+      buffer[i*PAGE_SIZE + 31] += i;
+      buffer[i*PAGE_SIZE + 168] += i;
     }
   }
 }
