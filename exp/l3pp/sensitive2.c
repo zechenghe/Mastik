ab8f5ca08194d42da66a6e9732667d97ce589912
zechengh <zcheustc@gmail.com>
2020-02-05 23:46:11 -0500

    sen2

--- exp/l3pp/sensitive2.c
+++ exp/l3pp/sensitive2.c
@@ -1,3 +1,7 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/mman.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -7,6 +11,7 @@
 volatile char buffer[PAGE_SIZE*1024];
 
 int main(int ac, char **av) {
+  buffer = mmap(0, npages * PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_HUGETLB, -1, 0);
   for (;;) {
     for (int i = 0; i < 1024; i++){
       buffer[i*PAGE_SIZE + 31] += i;
