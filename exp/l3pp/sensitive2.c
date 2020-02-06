8557a1b83be10c82bd0cf74886e8eafa9d91837d
zechengh <zcheustc@gmail.com>
2020-02-05 23:48:34 -0500

    sen2

--- exp/l3pp/sensitive2.c
+++ exp/l3pp/sensitive2.c
@@ -8,10 +8,14 @@
 
 #define PAGE_SIZE 4096
 
-volatile char buffer[PAGE_SIZE*1024];
-
 int main(int ac, char **av) {
-  buffer = mmap(0, npages * PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_HUGETLB, -1, 0);
+  int npages = 4096;
+  char* buffer = (char*)mmap(0, npages * PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_HUGETLB, -1, 0);
+  if (!buffer){
+    printf("mmap error");
+    exit(1);
+  }
+
   for (;;) {
     for (int i = 0; i < 1024; i++){
       buffer[i*PAGE_SIZE + 31] += i;
