d32de11cab655293a5c2b729ba1a27b1743ce964
zechengh <zcheustc@gmail.com>
2020-02-25 13:18:07 -0500

    read

--- exp/flush_reload/run_sensitive.sh
+++ exp/flush_reload/run_sensitive.sh
@@ -13,7 +13,7 @@ mkdir -p $OUTPUT_FOLDER
 rm -f $EXP_ROOT_DIR/flush_reload/results/*
 
 GPG=$ROOT_DIR/gnupg-1.4.13/g10/gpg
-SENSITIVE_PROGRAM=sensitive4
+SENSITIVE_PROGRAM=sensitive5
 INTERVAL_US=10000
 DATA_COLLECTION_TIME_S=100
 
@@ -23,7 +23,7 @@ do
   do
     HPC_SUFFIX=${HPC_COLLECTION}_${SPLIT}
     status "Sensitive program running"
-    taskset 0x8 ./$SENSITIVE_PROGRAM &
+    taskset 0x8 ./$SENSITIVE_PROGRAM $GPG&
     SENSITIVE_PID=$!
 
     taskset 0x10 $quickhpc -c hpc_config_$HPC_COLLECTION -a $SENSITIVE_PID -i $INTERVAL_US > $OUTPUT_FOLDER/hpc_sensiprog_$HPC_SUFFIX &
@@ -40,7 +40,7 @@ do
     SPY_PID=$!
 
     status "Sensitive program running"
-    taskset 0x8 ./$SENSITIVE_PROGRAM &
+    taskset 0x8 ./$SENSITIVE_PROGRAM $GPG&
     SENSITIVE_PID=$!
 
     taskset 0x10 $quickhpc -c hpc_config_$HPC_COLLECTION -a $SENSITIVE_PID -i $INTERVAL_US > $OUTPUT_FOLDER/hpc_sensiprog_abnormal_$HPC_SUFFIX &
