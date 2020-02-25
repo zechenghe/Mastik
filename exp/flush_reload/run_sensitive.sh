450bd6681c0aea4c5b9ce8b2ce67f504a5f11b51
zechengh <zcheustc@gmail.com>
2020-02-24 21:52:35 -0500

    fr attack

--- exp/flush_reload/run_sensitive.sh
+++ exp/flush_reload/run_sensitive.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 quickhpc="/home/zechengh/quickhpc/quickhpc"
 
 ROOT_DIR="/home/zechengh/Mastik"
@@ -8,35 +10,47 @@ source $EXP_ROOT_DIR/exp_funcs.sh
 
 OUTPUT_FOLDER=$EXP_ROOT_DIR/flush_reload/results
 mkdir -p $OUTPUT_FOLDER
+rm -f $EXP_ROOT_DIR/flush_reload/results/*
 
 GPG=$ROOT_DIR/gnupg-1.4.13/g10/gpg
+SENSITIVE_PROGRAM=sensitive4
+INTERVAL_US=10000
 
-status "Sensitive program running"
-taskset 0x80000 ./sensitive1 &
-SENSITIVE_PID=$!
-echo $SENSITIVE_PID
+for SPLIT in TRAINING TESTING
+do
+  for HPC_COLLECTION in L1 L23 INS
+  do
+    HPC_SUFFIX=${HPC_COLLECTION}_${SPLIT}
+    status "Sensitive program running"
+    taskset 0x8 ./$SENSITIVE_PROGRAM &
+    SENSITIVE_PID=$!
 
-$quickhpc -c hpc_config -a $SENSITIVE_PID -i 100 > $OUTPUT_FOLDER/hpc_sensiprog &
-QUICKHPC_PID=$!
+    taskset 0x10 $quickhpc -c hpc_config_$HPC_COLLECTION -a $SENSITIVE_PID -i $INTERVAL_US > $OUTPUT_FOLDER/hpc_sensiprog_$HPC_SUFFIX &
+    QUICKHPC_PID=$!
 
-sleep 5
-kill $QUICKHPC_PID
-kill $SENSITIVE_PID
+    sleep 100
+    kill $QUICKHPC_PID
+    kill $SENSITIVE_PID
 
+    sleep 2
 
-status "Spy running"
-taskset 0x20000 ./spy $GPG &
-SPY_PID=$!
+    status "Spy running"
+    taskset 0x2000 ./spy 100000000 &
+    SPY_PID=$!
 
-status "Sensitive program running"
-taskset 0x80000 ./sensitive1 &
-SENSITIVE_PID=$!
-echo $SENSITIVE_PID
+    status "Sensitive program running"
+    taskset 0x8 ./$SENSITIVE_PROGRAM &
+    SENSITIVE_PID=$!
 
-$quickhpc -c hpc_config -a $SENSITIVE_PID -i 100 > $OUTPUT_FOLDER/hpc_sensiprog_abnormal &
-QUICKHPC_PID=$!
+    taskset 0x10 $quickhpc -c hpc_config_$HPC_COLLECTION -a $SENSITIVE_PID -i $INTERVAL_US > $OUTPUT_FOLDER/hpc_sensiprog_abnormal_$HPC_SUFFIX &
+    QUICKHPC_PID=$!
 
-sleep 5
-kill $QUICKHPC_PID
-kill $SENSITIVE_PID
-kill $SPY_PID
+    sleep 100
+    kill $QUICKHPC_PID
+    kill $SENSITIVE_PID
+    kill $SPY_PID
+
+    sleep 2
+
+  done
+done
