2121fbe39b3f938bb7bd780cfc0f5cc6cbeb235e
zechengh <zcheustc@gmail.com>
2020-02-26 12:34:04 -0500

    mmap

--- exp/flush_reload/sensitive5.c
+++ exp/flush_reload/sensitive5.c
@@ -24,7 +24,9 @@ int nmonitor = sizeof(monitor)/sizeof(monitor[0]);
 int main(int argc, char **argv) {
   char temp = 0;
   int fd = open(argv[1], O_RDONLY);
-  char* buffer = (char*)mmap(0, NPAGES * PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_HUGETLB, fd, 0);
+
+  //mapaddress = mmap(0, sysconf(_SC_PAGE_SIZE), PROT_READ, MAP_PRIVATE, fd, offset & ~(sysconf(_SC_PAGE_SIZE) -1));
+  char* buffer = (char*)mmap(0, NPAGES * PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_HUGETLB, fd, 0);
   if (!buffer){
     printf("mmap error");
     exit(1);
