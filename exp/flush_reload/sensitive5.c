a9001a011a684f8346af104387e2a626632df207
zechengh <zcheustc@gmail.com>
2020-02-26 12:52:45 -0500

    readonly

--- exp/flush_reload/sensitive5.c
+++ exp/flush_reload/sensitive5.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
   int fd = open(argv[1], O_RDONLY);
 
   //mapaddress = mmap(0, sysconf(_SC_PAGE_SIZE), PROT_READ, MAP_PRIVATE, fd, offset & ~(sysconf(_SC_PAGE_SIZE) -1));
-  char* buffer = (char*)mmap(0, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_HUGETLB, fd, 0);
+  char* buffer = (char*)mmap(0, PAGE_SIZE, PROT_READ, MAP_PRIVATE|MAP_HUGETLB, fd, 0);
   if (!buffer){
     printf("mmap error");
     exit(1);
