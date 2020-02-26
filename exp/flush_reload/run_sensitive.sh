f259e2e8ad402fcb4ffcc3876dd5a342307c3e61
zechengh <zcheustc@gmail.com>
2020-02-25 23:29:32 -0500

    kill

--- exp/flush_reload/run_sensitive.sh
+++ exp/flush_reload/run_sensitive.sh
@@ -19,7 +19,7 @@ INTERVAL_US=100000
 DATA_COLLECTION_TIME_S=100
 
 ps -ef | awk '/sensitive6/ {print $2;}' | xargs kill
-ps -ef | grep $SPY_PROGRAM | awk '{print $2;}' | xargs kill
+#ps -ef | grep $SPY_PROGRAM | awk '{print $2;}' | xargs kill
 
 for SPLIT in TRAINING TESTING
 do
