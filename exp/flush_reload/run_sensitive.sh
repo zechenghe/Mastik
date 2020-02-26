2a289e7fb77ff5993b817f51a2338db0ef733740
zechengh <zcheustc@gmail.com>
2020-02-25 23:14:39 -0500

    print

--- exp/flush_reload/run_sensitive.sh
+++ exp/flush_reload/run_sensitive.sh
@@ -18,7 +18,7 @@ SPY_PROGRAM=./spy
 INTERVAL_US=100000
 DATA_COLLECTION_TIME_S=100
 
-SEN="$(ps -ef | grep $SENSITIVE_PROGRAM)"
+SEN="$(ps -ef | grep $SENSITIVE_PROGRAM) | awk '{print $2; print $8}'"
 echo $SEN
 ps -ef | grep spy | awk '{print $2;}'
 
