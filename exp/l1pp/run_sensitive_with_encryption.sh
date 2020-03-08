6a9a9a4e7e78a63e5b1e27ee63d904e1311d20f2
zechengh <zcheustc@gmail.com>
2020-03-08 18:19:17 -0400

    enc hpc

--- exp/l1pp/run_sensitive_with_encryption.sh
+++ exp/l1pp/run_sensitive_with_encryption.sh
@@ -11,19 +11,25 @@ mkdir -p $OUTPUT_FOLDER
 rm -f $EXP_ROOT_DIR/l1pp/results/*
 
 GPG=$ROOT_DIR/gnupg-1.4.13/g10/gpg
+SENSITIVE_PROGRAM=sensitive1
+SPY_PROGRAM=./spy
+INTERVAL_US=10000
+DATA_COLLECTION_TIME_S=10
 
 clean_env () {
-   echo "Killing processes quickhpc, sensitive[1-9], spy, gnupg"
-   ps -ef | grep "quickhpc" | awk '{print $2;}' | xargs -r kill
-   ps -ef | grep "sensitive[1-9]" | awk '{print $2;}' | xargs -r kill
-   ps -ef | grep "spy" | awk '{print $2;}' | xargs -r kill
-   ps -ef | grep "encrypt_" | awk '{print $2;}' | xargs -r kill
+    sleep 1
+    echo "Killing processes quickhpc, sensitive[1-9], spy, gnupg"
+    ps -ef | grep "quickhpc" | awk '{print $2;}' | xargs -r kill
+    ps -ef | grep "sensitive[1-9]" | awk '{print $2;}' | xargs -r kill
+    ps -ef | grep "spy" | awk '{print $2;}' | xargs -r kill
+    ps -ef | grep "encrypt_" | awk '{print $2;}' | xargs -r kill
+    sleep 1
 }
 
 
 for SPLIT in TRAINING TESTING
 do
-  for HPC_COLLECTION in L1 L23 MISC
+  for HPC_COLLECTION in L1 L23 INS
   do
     HPC_SUFFIX=${HPC_COLLECTION}_${SPLIT}
     status "Sensitive program running"
@@ -33,7 +39,6 @@ do
     ./encrypt_rsa.sh &
 
     $quickhpc -c hpc_config_$HPC_COLLECTION -a $SENSITIVE_PID -i 1000 > $OUTPUT_FOLDER/hpc_sensiprog_$HPC_SUFFIX &
-    QUICKHPC_PID=$!
 
     sleep 10
 
@@ -53,6 +58,54 @@ do
     QUICKHPC_PID=$!
 
     sleep 10
+
+    clean_env
+
+  done
+done
+
+
+
+
+
+
+for SPLIT in TRAINING TESTING
+do
+  for HPC_COLLECTION in L1 L23 INS
+  do
+    HPC_SUFFIX=${HPC_COLLECTION}_${SPLIT}
+
+    status "Encryption running"
+    ./encrypt_rsa.sh &
+
+    status "Sensitive program running"
+    taskset 0x8 ./$SENSITIVE_PROGRAM &
+    SENSITIVE_PID=$!
+
+    sleep 1
+
+    taskset 0x10 $quickhpc -c hpc_config_$HPC_COLLECTION -a $SENSITIVE_PID -i $INTERVAL_US > $OUTPUT_FOLDER/hpc_sensiprog_$HPC_SUFFIX &
+
+    sleep $DATA_COLLECTION_TIME_S
+    clean_env
+
+
+
+    status "Encryption running"
+    ./encrypt_rsa.sh &
+
+    status "Sensitive program running"
+    taskset 0x8 ./$SENSITIVE_PROGRAM &
+    SENSITIVE_PID=$!
+
+    status "Spy running"
+    taskset 0x8000 ./spy 1000000000 &
+
+    sleep 1
+
+    taskset 0x10 $quickhpc -c hpc_config_$HPC_COLLECTION -a $SENSITIVE_PID -i $INTERVAL_US > $OUTPUT_FOLDER/hpc_sensiprog_abnormal_$HPC_SUFFIX &
+
+    sleep $DATA_COLLECTION_TIME_S
 
     clean_env
 
