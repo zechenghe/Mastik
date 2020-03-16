5715a77d0492cc362fd982c3b088a2ecbb215450
zechengh <zcheustc@gmail.com>
2020-03-16 13:32:29 -0400

    testrun

--- exp/test_workspace/run_sensitive_progs_together_with_enc.sh
+++ exp/test_workspace/run_sensitive_progs_together_with_enc.sh
@@ -55,7 +55,7 @@ do
     ./encrypt_rsa.sh &
 
     spawn_sensitive_programs
-    for i in "${!SPs[@]}"
+    for i in ${!SPs[@]}
     do
         HPC_SUFFIX=${!SPs[i]}_${HPC_COLLECTION}_${SPLIT}
         taskset 0x10 $quickhpc -c hpc_config_$HPC_COLLECTION -a ${!SPIDs[i]} -i $INTERVAL_US > $OUTPUT_FOLDER/hpc_$HPC_SUFFIX &
@@ -72,7 +72,7 @@ do
     taskset 0x2000 $SPY_PROGRAM $GPG &
 
     spawn_sensitive_programs
-    for i in "${!SPs[@]}"
+    for i in ${!SPs[@]}
     do
         HPC_SUFFIX=${!SPs[i]}_${HPC_COLLECTION}_${SPLIT}_abnormal
         taskset 0x10 $quickhpc -c hpc_config_$HPC_COLLECTION -a ${!SPIDs[i]} -i $INTERVAL_US > $OUTPUT_FOLDER/hpc_$HPC_SUFFIX &
