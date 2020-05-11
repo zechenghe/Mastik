e026b42949aaa17d6820efe870305dac753d0f08
zechengh <zcheustc@gmail.com>
2020-05-11 16:19:16 -0400

    ls

--- exp/test_workspace/run_sensitive_spec.sh
+++ exp/test_workspace/run_sensitive_spec.sh
@@ -71,7 +71,7 @@ encrypt_large_file (){
 
 clean_env
 
-SPEC=perlbench
+SPEC="perlbench"
 spec_background "$SPEC"
 
 for SPEC in perlbench none bzip2 gcc mcf milc namd gobmk soplex povray hmmer sjeng libquantum h264ref lbm omnetpp astar
