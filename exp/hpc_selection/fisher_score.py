8393397f41965e1bf3e508feb9b156ef61ac8189
zechengh <zcheustc@gmail.com>
2020-05-22 12:03:38 -0400

    fisher score

---
+++ exp/hpc_selection/fisher_score.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def read_file(path):
+    f = open(path, 'r')
+    return np.array(list(f))
+
+data_dir = 'results/'
+SP = 'sensitive5'
+
+for hpc in ['BR_CN', 'BR_INS', 'BR_MSP', 'BR_NTK', 'BR_PRC', 'FP_INS', 'L1_DCM', 'L1_ICM',
+            'L1_LDM', 'L1_STM', 'L1_TCM', 'L2_TCA', 'L2_TCM', 'L3_TCA', 'L3_TCM', 'LD_INS',
+            'SR_INS', 'STL_ICY', 'TLB_DM', 'TLB_IM', 'TOT_CYC', 'TOT_INS']:
+
+    neg1_path = data_dir + '{SP}_{hpc}_none1'.format{
+        SP = SP,
+        hpc = hpc
+    }
+    neg2_path = data_dir + '{SP}_{hpc}_none2'.format{
+        SP = SP,
+        hpc = hpc
+    }
+    pos1_path = data_dir + '{SP}_{hpc}_flush'.format{
+        SP = SP,
+        hpc = hpc
+    }
+    pos2_path = data_dir + '{SP}_{hpc}_l3prime'.format{
+        SP = SP,
+        hpc = hpc
+    }
+
+    neg1 = read_file(neg1_path)
+    neg2 = read_file(neg2_path)
+    pos1 = read_file(pos1_path)
+    pos2 = read_file(pos2_path)
+
+    print neg1.shape, neg2.shape, pos1.shape, pos2.shape
