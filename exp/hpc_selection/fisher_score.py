93597503f826d5932345156b5f17a613f16a1c1f
zechengh <zcheustc@gmail.com>
2020-05-22 12:04:27 -0400

    format

--- exp/hpc_selection/fisher_score.py
+++ exp/hpc_selection/fisher_score.py
@@ -11,26 +11,26 @@ for hpc in ['BR_CN', 'BR_INS', 'BR_MSP', 'BR_NTK', 'BR_PRC', 'FP_INS', 'L1_DCM',
             'L1_LDM', 'L1_STM', 'L1_TCM', 'L2_TCA', 'L2_TCM', 'L3_TCA', 'L3_TCM', 'LD_INS',
             'SR_INS', 'STL_ICY', 'TLB_DM', 'TLB_IM', 'TOT_CYC', 'TOT_INS']:
 
-    neg1_path = data_dir + '{SP}_{hpc}_none1'.format{
+    neg1_path = data_dir + '{SP}_{hpc}_none1'.format(
         SP = SP,
         hpc = hpc
-    }
-    neg2_path = data_dir + '{SP}_{hpc}_none2'.format{
+    )
+    neg2_path = data_dir + '{SP}_{hpc}_none2'.format(
         SP = SP,
         hpc = hpc
-    }
-    pos1_path = data_dir + '{SP}_{hpc}_flush'.format{
+    )
+    pos1_path = data_dir + '{SP}_{hpc}_flush'.format(
         SP = SP,
         hpc = hpc
-    }
-    pos2_path = data_dir + '{SP}_{hpc}_l3prime'.format{
+    )
+    pos2_path = data_dir + '{SP}_{hpc}_l3prime'.format(
         SP = SP,
         hpc = hpc
-    }
+    )
 
     neg1 = read_file(neg1_path)
     neg2 = read_file(neg2_path)
     pos1 = read_file(pos1_path)
     pos2 = read_file(pos2_path)
 
-    print neg1.shape, neg2.shape, pos1.shape, pos2.shape
+    print neg1.shape, neg2.shape, pos1.shape, pos2.shapeÓ
