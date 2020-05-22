5faf658c5b62272e2d94c3ac63481e8942c3d453
zechengh <zcheustc@gmail.com>
2020-05-22 14:55:56 -0400

    fisher score

--- exp/hpc_selection/fisher_score.py
+++ exp/hpc_selection/fisher_score.py
@@ -3,6 +3,15 @@ import numpy as np
 def read_file(path):
     return np.loadtxt(path)
 
+def f_score(pos, neg):
+    neg_mean = np.mean(neg)
+    neg_var = np.var(neg)
+    pos_mean = np.mean(pos)
+    pos_var = np.var(pos)
+
+    return ((pos_mean-neg_mean) ** 2) / (len(neg)*neg_var+len(pos)*pos_var)
+
+
 data_dir = 'results/'
 SP = 'sensitive5'
 
@@ -32,16 +41,8 @@ for hpc in ['BR_CN', 'BR_INS', 'BR_MSP', 'BR_NTK', 'BR_PRC', 'FP_INS', 'L1_DCM',
     pos1 = read_file(pos1_path)
     pos2 = read_file(pos2_path)
 
-    neg = np.concatenate((neg1, neg2), axis=0)
-    pos = np.concatenate((pos1, pos2), axis=0)
+    f1 = f_score(neg1, pos1)
+    f2 = f_score(neg2, pos2)
+    fisher_score = (f1+f2) / 2
 
-    print neg.shape
-    print pos.shape
-
-    neg_mean = np.mean(neg)
-    neg_var = np.var(neg)
-    pos_mean = np.mean(pos)
-    pos_var = np.var(pos)
-
-    fisher_score = ((pos_mean-neg_mean) ** 2) / (neg_var+pos_var)
     print hpc, fisher_score
