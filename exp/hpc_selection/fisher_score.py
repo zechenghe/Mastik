a5bf9ed122a77468b3f0b8fc5545bb5a33ad739e
zechengh <zcheustc@gmail.com>
2020-05-23 13:26:23 -0400

    fisher_score

--- exp/hpc_selection/fisher_score.py
+++ exp/hpc_selection/fisher_score.py
@@ -60,7 +60,7 @@ def collect_f_score(collection, data_dir='results/', SP='sensitive5'):
 data_dirs = os.listdir('archive/')
 data_dir_filtered = []
 for d in data_dirs:
-    if d >= '20200523_020440':
+    if d >= '20200523_120440':
         data_dir_filtered.append(d)
 
 
