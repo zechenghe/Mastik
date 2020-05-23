74b4aacc67f4b099aea054b6c007757f8c3c9c25
zechengh <zcheustc@gmail.com>
2020-05-23 13:27:46 -0400

    fisher_score

--- exp/hpc_selection/fisher_score.py
+++ exp/hpc_selection/fisher_score.py
@@ -50,8 +50,8 @@ def collect_f_score(collection, data_dir='results/', SP='sensitive5'):
         f1 = f_score(neg1, pos1)
         f2 = f_score(neg2, pos2)
 
-        fisher_score = f1
-        #fisher_score = (f1+f2) / 2
+        #fisher_score = f1
+        fisher_score = (f1+f2) / 2
 
         #print hpc, f1, f2, fisher_score
         collection[hpc] += fisher_score
