0efd3f580aea4c29a47a41456faf1dc966560986
zechengh <zcheustc@gmail.com>
2020-05-27 01:45:03 -0400

    config

--- exp/test_workspace/collect_data.sh
+++ exp/test_workspace/collect_data.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+
+
 # 0-a. Encryption
 # 0-b. Encryption, attack
 # 1. Sensitive prog, encryption
 # 2. Sensitive prog, encryption, attack
 # 3. Sensitive prog, encryption, backgournd(benchmark)
 # 4. Sensitive prog, encryption, backgournd(benchmark), attack
-
 
 ./collect_data_0a.sh
 ./collect_data_0b.sh
