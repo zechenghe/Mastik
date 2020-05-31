c6f03aebd618e3f12d1970d9eb4cbc2cca2346f1
zechengh <zcheustc@gmail.com>
2020-05-31 17:40:18 -0400

    enc

--- exp/test_workspace/encrypt_rsa.sh
+++ exp/test_workspace/encrypt_rsa.sh
@@ -6,6 +6,6 @@ source ./config.sh
 
 while true
   do
-    $GPG --batch -r zechengh_key1 -o /dev/null -e 'hello'
+    taskset 0x8 $GPG --batch -r zechengh_key1 -o /dev/null -e 'hello'
     sleep 0.01
   done
