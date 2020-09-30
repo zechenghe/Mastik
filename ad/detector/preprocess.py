55af9803782f0ed7bcf1059d3d13fb0b80f1e690
zechengh <zcheustc@gmail.com>
2020-09-30 17:13:23 -0400

    preprocess

--- ad/detector/preprocess.py
+++ ad/detector/preprocess.py
@@ -8,10 +8,15 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', type = str, default = "../perf/data/core0/100us/", help='The directory of data')
+parser.add_argument('--file_name', type = str, default = None, help='The directory of data')
 args = parser.parse_args()
 
-for f in os.listdir(data_dir):
-    extension = f.split('.')[-1]
-    if extension == 'csv':
-        data = utils.read_csv_file(data_dir+f)
-        np.save(data_dir + "".join(f.split('.')[:-1]) + '.npy', data)
+if file_name != None:
+    data = utils.read_csv_file(data_dir+file_name)
+    np.save(data_dir + "".join(file_name.split('.')[:-1]) + '.npy', data)
+else:
+    for f in os.listdir(data_dir):
+        extension = f.split('.')[-1]
+        if extension == 'csv':
+            data = utils.read_csv_file(data_dir+f)
+            np.save(data_dir + "".join(f.split('.')[:-1]) + '.npy', data)
