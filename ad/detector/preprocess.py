c0a9fb203a2b7102e3226c689de2f4802da5ece7
zechengh <zcheustc@gmail.com>
2020-10-03 17:50:06 -0400

    remove outlier

--- ad/detector/preprocess.py
+++ ad/detector/preprocess.py
@@ -8,6 +8,14 @@ import os
 
 import utils
 
+def remove_outlier(data):
+    mu = np.mean(data, axis=0)
+    std = np.mean(data, axis=0)
+
+    th = 3 * std
+    data = (np.abs(data-mu) > th) * mu + (np.abs(data-mu) <= th) * data
+    return data
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', type = str, default = "../perf/data/core0/100us/", help='The directory of data')
 parser.add_argument('--file_name', type = str, default = None, help='The directory of data')
@@ -17,10 +25,12 @@ data_dir = args.data_dir
 file_name = args.file_name
 if file_name != None:
     data = utils.read_csv_file(data_dir+file_name)
+    data = remove_outlier(data)
     np.save(data_dir + "".join(file_name.split('.')[:-1]) + '.npy', data)
 else:
     for f in os.listdir(data_dir):
         extension = f.split('.')[-1]
         if extension == 'csv':
             data = utils.read_csv_file(data_dir+f)
+            data = remove_outlier(data)
             np.save(data_dir + "".join(f.split('.')[:-1]) + '.npy', data)
