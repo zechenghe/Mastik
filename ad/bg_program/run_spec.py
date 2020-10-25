3e7ee3dee7b9e88b26db04eca6fc7bc6d9aac510
zechengh <zcheustc@gmail.com>
2020-10-25 12:24:45 -0400

    spec

--- ad/bg_program/run_spec.py
+++ ad/bg_program/run_spec.py
@@ -3,18 +3,33 @@
 import os
 import subprocess
 import time
+import random
 
 def spec_cmd(spec_prog):
     return "taskset 0x1 runspec --config=test.cfg --size=train" \
     " --noreportable --tune=base --iterations=1 {spec_prog}".format(
         spec_prog=spec_prog)
 
+spec_benchmarks = ('perlbench', 'bzip2', 'gcc', 'mcf', 'milc', 'namd',
+'gobmk', 'soplex', 'povray', 'hmmer', 'sjeng', 'libquantum',
+'h264ref', 'lbm', 'omnetpp', 'astar')
+
 while True:
-    for spec_prog in ('perlbench', 'bzip2', 'gcc', 'mcf', 'milc', 'namd',
-    'gobmk', 'soplex', 'povray', 'hmmer', 'sjeng', 'libquantum',
-    'h264ref', 'lbm', 'omnetpp', 'astar'):
+    num_spec_processes = random.randint(1, 5)
+
+    benchmark_run = []
+    for i in range(num_spec_processes):
+        benchmark_idx = random.randrange(len(spec_benchmarks))
+        benchmark_run.append(spec_benchmarks[benchmark_idx])
+    print("Run {l} benchmarks:".format(l=len(benchmark_run)), benchmark_run)
+
+    running_processes = []
+    for spec_prog in benchmark_run:
         cmd = spec_cmd(spec_prog)
         print(cmd)
         spec_process = subprocess.Popen(cmd.split())
-        spec_status = spec_process.wait()
-        time.sleep(1)
+        running_processes.append(spec_process)
+
+    for p in running_processes:
+        p.wait()
+    time.sleep(1)
