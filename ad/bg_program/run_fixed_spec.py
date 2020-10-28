62c5c37c1df81610dbfb573be0df582fb86e342c
zechengh <zcheustc@gmail.com>
2020-10-28 17:48:26 -0400

    spec

---
+++ ad/bg_program/run_fixed_spec.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2
+
+import os
+import subprocess
+import time
+import random
+
+def spec_cmd(spec_prog):
+    return "taskset 0x1 runspec --config=test.cfg --size=train" \
+    " --noreportable --tune=base --iterations=1 {spec_prog}".format(
+        spec_prog=spec_prog)
+
+spec_benchmarks = ('perlbench', 'bzip2', 'gcc', 'mcf', 'milc', 'namd',
+'gobmk', 'soplex', 'povray', 'hmmer', 'sjeng', 'libquantum',
+'h264ref', 'lbm', 'omnetpp', 'astar')
+
+while True:
+
+    running_processes = []
+    for spec_prog in ['perlbench', 'gcc', 'libquantum']:
+        cmd = spec_cmd(spec_prog)
+        print(cmd)
+        spec_process = subprocess.Popen(cmd.split())
+        running_processes.append(spec_process)
+
+    for p in running_processes:
+        p.wait()
+    time.sleep(1)
