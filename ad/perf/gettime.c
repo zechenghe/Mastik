2944640bb4421b217b5c994d2a88335e2ba710f3
zechengh <zcheustc@gmail.com>
2020-11-09 16:46:40 -0500

    time

--- ad/perf/gettime.c
+++ ad/perf/gettime.c
@@ -4,12 +4,7 @@
 
 
 int main(void) {
-    struct timeval stop, start;
-    gettimeofday(&start, NULL);
-    //do stuff
-    gettimeofday(&stop, NULL);
-
-    printf("start.tv_sec %lu \n", start.tv_sec);
-    printf("start.tv_usec %lu \n", start.tv_usec);
-    printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    print("%f \n", t.tv_sec + (float)(t.tv_usec) / 1000000)
 }
