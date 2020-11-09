016fd308a861bf606a7cccbcb9861bc9a99306be
zechengh <zcheustc@gmail.com>
2020-11-09 16:32:46 -0500

    ain

--- ad/perf/gettime.c
+++ ad/perf/gettime.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct timeval stop, start;
-gettimeofday(&start, NULL);
-//do stuff
-gettimeofday(&stop, NULL);
 
-printf("start.tv_sec %lu \n", start.tv_sec);
-printf("start.tv_usec %lu \n", start.tv_usec);
-printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+int main(void) {
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    //do stuff
+    gettimeofday(&stop, NULL);
+
+    printf("start.tv_sec %lu \n", start.tv_sec);
+    printf("start.tv_usec %lu \n", start.tv_usec);
+    printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+}
