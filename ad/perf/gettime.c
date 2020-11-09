b9b028c2dbb3d4613d5a78f6fadfb32757ee8bb9
zechengh <zcheustc@gmail.com>
2020-11-09 16:50:17 -0500

    print time

--- ad/perf/gettime.c
+++ ad/perf/gettime.c
@@ -6,7 +6,5 @@
 int main(void) {
     struct timeval t;
     gettimeofday(&t, NULL);
-    printf("%f \n", t.tv_sec);
-    printf("%f %f\n", t.tv_usec, (float)(t.tv_usec) / 1000000);
-    printf("%f \n", t.tv_sec + (float)(t.tv_usec) / 1000000);
+    printf("%f \n", t.tv_sec + (float)(t.tv_usec));
 }
