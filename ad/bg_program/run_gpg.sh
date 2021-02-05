717ac3a5d25393f92177318fa4370ee33ec2f0bd
zechengh <zcheustc@gmail.com>
2021-02-04 19:23:07 -0500

    gpg script

--- ad/bg_program/run_gpg.sh
+++ ad/bg_program/run_gpg.sh
@@ -4,5 +4,6 @@ set -u
 
 while true
   do
-    gpg --batch -r zechengh_key1 -o /dev/null -e /home/zechengh/sample.txt
+    taskset 0x8 gpg --batch -r zechengh_key1 -e /home/zechengh/sample.txt
+    sleep 1
   done
