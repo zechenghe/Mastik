9db00c37d4373671f13dccbef1e0ee493ec24be3
zechengh <zcheustc@gmail.com>
2021-02-04 22:37:05 -0500

    yes

--- ad/bg_program/run_gpg.sh
+++ ad/bg_program/run_gpg.sh
@@ -4,6 +4,6 @@ set -u
 
 while true
   do
-    taskset 0x8 gpg --batch -r zechengh_key1 -e /home/zechengh/sample.txt
+    taskset 0x8 gpg --yes --batch -r zechengh_key1 -e /home/zechengh/sample.txt
     sleep 1
   done
