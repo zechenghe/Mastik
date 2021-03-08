bff8f1227deac074bc839001af4902b638bb06e9
zechengh <zcheustc@gmail.com>
2021-03-07 19:23:24 -0500

    mapreduce

--- ad/bg_program/mapreduce.sh
+++ ad/bg_program/mapreduce.sh
@@ -5,6 +5,6 @@ set -u
 $HADOOP_HOME/bin/hadoop fs -rm -r /user/zechengh/terasort/out
 while true
   do
-    taskset 0x8 $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.0.jar terasort /user/zechengh/terasort/randinputlarge/ /user/zechengh/terasort/out;
+    $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.0.jar terasort /user/zechengh/terasort/randinputlarge/ /user/zechengh/terasort/out;
     $HADOOP_HOME/bin/hadoop fs -rm -r /user/zechengh/terasort/out
   done
