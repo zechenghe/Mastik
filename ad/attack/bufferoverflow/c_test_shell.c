7b91d071c8a8d5a3779cbdffc0bd3f26e45fce20
zechengh <zcheustc@gmail.com>
2021-03-19 13:57:12 -0400

    c

--- ad/attack/bufferoverflow/c_test_shell.c
+++ ad/attack/bufferoverflow/c_test_shell.c
@@ -10,17 +10,17 @@
 
 int main(int argc, char **argv)
 {
-    __asm__(
-        "movq    rcx, 1"
-        "movq    rax, 59"                /* syscall arg 1: syscall number execve(59) */
-        "movq    %rcx, %rdi"               /* syscall arg 2: string pathname */
-        "leaq    8(%rcx), %rsi"            /* syscall arg 2: argv ptr to ['/bin/sh']*/
-        "movq    $0, %rdx"                 /* syscall arg 4: envp (NULL) */
-        "syscall"                          /* Call execve("/bin/sh", ["/bin/sh"], []) */
+    __asm(
+        "movq    rcx, 1\n"
+//        "movq    rax, 59"                /* syscall arg 1: syscall number execve(59) */
+//        "movq    %rcx, %rdi"               /* syscall arg 2: string pathname */
+//        "leaq    8(%rcx), %rsi"            /* syscall arg 2: argv ptr to ['/bin/sh']*/
+//        "movq    $0, %rdx"                 /* syscall arg 4: envp (NULL) */
+//        "syscall"                          /* Call execve("/bin/sh", ["/bin/sh"], []) */
 
-        "movq    $60, %rax"                /* syscall arg 1: SYS_exit (60) */
-        "xorq    %rdi,%rdi"                /* syscall arg 2: 0 */
-        "syscall"                          /* invoke syscall */
+//        "movq    $60, %rax"                /* syscall arg 1: SYS_exit (60) */
+//        "xorq    %rdi,%rdi"                /* syscall arg 2: 0 */
+//        "syscall"                          /* invoke syscall */
     );
 
     //execv("/bin/sh", NULL, NULL);
