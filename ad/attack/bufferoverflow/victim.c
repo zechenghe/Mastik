6bbc5e140499231a25fd6bca94c67a51aa33527a
zechengh <zcheustc@gmail.com>
2021-03-20 18:17:11 -0400

    print

--- ad/attack/bufferoverflow/victim.c
+++ ad/attack/bufferoverflow/victim.c
@@ -21,7 +21,7 @@ int vulnerable_func(char *str)
 	char buffer[72];
     //void *addr;
     //addr = get_sp();
-    //printf("%p", buffer);
+    printf("%p", buffer);
 
     /* The following strcpy function has a buffer overflow problem */
 
