1043c408a11409c67a9e1ac6ad71d1dc1f256686
zechengh <zcheustc@gmail.com>
2021-03-21 12:49:24 -0400

    buffer

--- ad/bg_program/run_bufferoverflow.sh
+++ ad/bg_program/run_bufferoverflow.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# If attack not success (e.g., segmentation fault),
+# change BUFFER_PTR_L in payload.S to the printed address (buffer_addr).
+# Then 'make payload'
+
 set -u
 
 while true
